2a5685e743bcedb9823a28e349ff10cf2dad78da
kimi-ell <60607789+kimi-ell@users.noreply.github.com>
2020-07-18 16:58:17 +0300

    Add files via upload

--- js/script/main.js
+++ js/script/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    //всплывающее меню
     $('.header-nav-btn__menu').click(function(event) {
         $('.header-nav-btn__menu,.header-nav-menu').toggleClass('active');
         $('body').toggleClass('lock');
@@ -8,12 +10,22 @@ $(document).ready(function() {
         $('body').removeClass('lock')
     });
 
+    //спойлеры в подвале
     $('.footer-body-right-title').click(function(event){
         if($('.footer-body-right-row').hasClass('one')){
             $('.footer-body-right-title').not(this).removeClass('active__list');
             $('.footer-body-right-list').not($(this).next()).slideUp(300);
         }
         $(this).toggleClass('active__list').next().slideToggle(300);
+    });
+
+    //слайдер
+    $('.slide-people').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
     });
         
 });
