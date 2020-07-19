7b2c90085cce24e5a1c41f72e892ca7d204df041
kimi-ell <60607789+kimi-ell@users.noreply.github.com>
2020-07-19 17:05:27 +0300

    Update main.js

--- js/script/main.js
+++ js/script/main.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
     //фиксированная шапка
-    //const navOffset = $('.nav').offset().top;
     var lastScrollTop = 0;
     $(window).scroll(function(event){
         var st = $(this).scrollTop();
         if (st > lastScrollTop){
-            $('.header').removeClass('nav-fixed');
         }
         else {
             $('.header').addClass('nav-fixed');
         }
         lastScrollTop = st;
-        // const scrolled = $(this).scrollTop();
-        // if (scrolled > navOffset) {
-        //     $('.header').addClass('nav-fixed');
-        // }
-        // else if (scrolled < navOffset) {
-        //     $('.header').removeClass('nav-fixed');
-        // } 
     });
     $(window).scroll(function(event){
         var st = $(this).scrollTop();
